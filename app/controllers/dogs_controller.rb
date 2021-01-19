d0fa5681da10eac3a51ede733576957cd2587e3d
ethanwolfe10 <ethanwolfe@comcast.net>
2021-01-18 16:50:43 -0800

    created controllers for new dog submission and dog show view

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,16 +1,35 @@
 class DogsController < ApplicationController
 
-    get '/dogs/:id' do
-        #if logged in view all doctor's dogs
+    get '/dogs/:slug' do
+        if session[:user_id]
+            @dog = Dog.find_by(id: params[:id])
+            @current_doctor = Doctor.find_by(id: session[:user_id])
+            erb :'/dogs/show'
+        else
+            redirect '/doctors/login'
+        end
     end
 
     get '/dogs/new' do
         #if logged in, view the new dog view
         #show all shelters or option to create a new shelter
+        if session[:user_id]
+            @current_doctor = Doctor.find_by(id: session[:user_id])
+            erb :'/dogs/new'
+        else
+            redirect '/doctors/login'
+        end
     end
 
     post '/dogs' do
         #create a new dog with params
+        if session[:user_id]
+            @new_dog = Dog.create(params)
+            @new_dog.doctor_ids << Doctor.find_by(id: session[:user_id])
+            redirect "/dogs/#{@new_dog.slug}"
+        else
+            redirect '/doctors/login'
+        end
     end
 
     get '/dogs/:id/edit' do
