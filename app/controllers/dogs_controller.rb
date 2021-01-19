c9f6f926254a3ecd54f83cc9764fd3ae84b0288c
ethanwolfe10 <ethanwolfe@comcast.net>
2021-01-18 20:17:23 -0800

    fixed issure where when new breed was created, new dog wouldnt be associated

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -15,7 +15,7 @@ class DogsController < ApplicationController
 
     get '/dogs/:slug' do
         if session[:user_id]
-            @dog = Dog.find_by(id: params[:id])
+            @dog = Dog.find_by_slug(params[:slug])
             @current_doctor = Doctor.find_by(id: session[:user_id])
             erb :'/dogs/show'
         else
@@ -24,10 +24,9 @@ class DogsController < ApplicationController
     end
 
     post '/dogs' do
-        #create a new dog with params
+        
         if session[:user_id]
             if params["shelter"]["shelter_name"] != ''
-                binding.pry
                 #need to check to see if shelter is already in database before creating a new one
                 new_shelter = Shelter.create(name: params["shelter"]["shelter_name"], website: params["shelter"]["shelter_website"], address: params["shelter"]["shelter_address"])
                 new_shelter.save
@@ -40,11 +39,10 @@ class DogsController < ApplicationController
                 else
                     new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: params["breed_id"], shelter_id: new_shelter.id)
                     new_dog.save
-                end  
-            else
-                binding.pry                
-                if !params["breed_name"] == ''
-                    # binding.pry
+                end
+                new_dog.doctor_ids = session[:user_id]  
+            else          
+                if params["breed_name"] != ''
                     new_breed = Breed.create(name: params["breed_name"])
                     new_breed.save
                     new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: new_breed.id, shelter_id: params["shelter_id"])
@@ -52,10 +50,10 @@ class DogsController < ApplicationController
                 else
                     new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: params["breed_id"], shelter_id: params["shelter_id"])
                     new_dog.save
-                end 
+                end
+                new_dog.doctor_ids = session[:user_id] 
             end
-            binding.pry
-            redirect "/dogs/#{@new_dog.slug}"
+            redirect "/dogs/#{new_dog.slug}"
         else
             redirect '/doctors/login'
         end
