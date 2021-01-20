85c4e895880a84e2689f77c506ad20040e54ae7d
ethanwolfe10 <ethanwolfe@comcast.net>
2021-01-19 16:19:06 -0800

    fixed how appointments are created and looking to add date feature

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,18 +1,41 @@
 class AppointmentsController < ApplicationController
-
-    get '/appointments/:id' do
-        #if logged in view all appts for doctor
-    end
-
+    
     get '/appointments/new' do
         #if logged in view new appt page
+        if session[:user_id]
+            @dogs = []
+            @current_doctor = Doctor.find_by(id: session[:user_id])
+            @shelters = Shelter.all.select {|shelter| shelter.doctors.include?(@current_doctor)}
+            @shelters.each do |shelter|
+                @dogs = shelter.dogs
+            end
+            erb :'/appointments/new'
+        else
+            redirect '/doctors/login'
+        end
+    end
+
+    get '/appointments/:slug' do
+        #if logged in view all appts for doctor
+        if session[:user_id]
+            @current_doctor = Doctor.find_by_slug(params[:slug])
+            
+        end
     end
 
     post '/appointments' do
         #creates appt with doctor_id and dog_id
+        if session[:user_id]
+            @current_doctor = Doctor.find_by(id: session[:user_id])
+           new_appt = DoctorDog.create(doctor_id: session[:user_id], dog_id: params["appt_dog"])
+           binding.pry
+           redirect "/appointments/#{@current_doctor.slug}"
+        else
+            redirect '/doctors/login'
+        end
     end
 
-    get '/appointments/:id/edit' do
+    get '/appointments/:slug/edit' do
         #if logged in view the appt edit page
     end
 
