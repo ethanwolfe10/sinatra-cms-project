4cb81534745afa604d29200493cda71df1ecc669
ethanwolfe10 <ethanwolfe@comcast.net>
2021-01-21 23:40:08 -0800

    completely restructured if statemtent that dealt with creating dogs/breeds/shelters

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -17,11 +17,8 @@ class DogsController < ApplicationController
     end
 
     get '/dogs/new' do
-        #if logged in, view the new dog view
-        #show all shelters or option to create a new shelter
         if session[:user_id]
             @current_doctor = Doctor.find_by(id: session[:user_id])
-            binding.pry
             @shelters = Shelter.all
             @breeds = Breed.all
             erb :'/dogs/new'
@@ -32,6 +29,7 @@ class DogsController < ApplicationController
 
     get '/dogs/:slug' do
         if session[:user_id]
+            binding.pry
             @dog = Dog.find_by_slug(params[:slug])
             @current_doctor = Doctor.find_by(id: session[:user_id])
             erb :'/dogs/show'
@@ -40,36 +38,83 @@ class DogsController < ApplicationController
         end
     end
 
+    # post '/dogs' do
+    #     current_doctor = Doctor.find_by(id: session[:user_id])
+    #     if session[:user_id]
+    #         if params["shelter"]["shelter_name"] == ''
+    #             #need to check to see if shelter is already in database before creating a new one
+    #             new_shelter = Shelter.create(name: params["shelter"]["shelter_name"], website: params["shelter"]["shelter_website"], address: params["shelter"]["shelter_address"])
+    #             new_shelter.save
+    #             new_shelter.doctors << current_doctor
+    #             if !params["breed_name"] == ''
+    #                 #need to check to see if breed is already in database before creating a new one
+    #                binding.pry
+    #                 new_breed = Breed.create(name: params["breed_name"])
+    #                 new_breed.save
+    #                 new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: new_breed.id, shelter_id: new_shelter.id)
+    #                 new_dog.save
+    #             else
+    #                 binding.pry
+    #                 new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: params["breed_id"], shelter_id: new_shelter.id)
+    #                 new_dog.save
+    #             end  
+    #         else
+    #             selected_shelter = Shelter.find_by(id: params["shelter_id"])
+    #             if !selected_shelter.doctors.include?(current_doctor)
+    #                 selected_shelter.doctors << current_doctor
+    #             end          
+    #             if !params["breed_name"] == ''
+    #                 binding.pry
+    #                 new_breed = Breed.create(name: params["breed_name"])
+    #                 new_breed.save
+    #                 new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: new_breed.id, shelter_id: params["shelter_id"])
+    #                 new_dog.save
+    #             else
+    #                 binding.pry
+    #                 new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: params["breed_id"], shelter_id: params["shelter_id"])
+    #                 new_dog.save
+    #             end 
+    #         end
+    #         redirect "/dogs/#{new_dog.slug}"
+    #     else
+    #         redirect '/doctors/login'
+    #     end
+    # end
+
     post '/dogs' do
-        current_doctor = Doctor.find_by(id: session[:user_id])
         if session[:user_id]
-            if params["shelter"]["shelter_name"] != ''
-                #need to check to see if shelter is already in database before creating a new one
-                new_shelter = Shelter.create(name: params["shelter"]["shelter_name"], website: params["shelter"]["shelter_website"], address: params["shelter"]["shelter_address"])
-                new_shelter.save
-                selected_shelter.doctors << current_doctor
-                if !params["breed_name"] == ''
-                    #need to check to see if breed is already in database before creating a new one
-                    new_breed = Breed.create(name: params["breed_name"])
-                    new_breed.save
-                    new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: new_breed.id, shelter_id: new_shelter.id)
-                    new_dog.save
-                else
-                    new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: params["breed_id"], shelter_id: new_shelter.id)
-                    new_dog.save
-                end  
-            else
+            current_doctor = Doctor.find_by(id: session[:user_id])
+            if params["shelter"]["shelter_name"] == ""
+                binding.pry
                 selected_shelter = Shelter.find_by(id: params["shelter_id"])
                 if !selected_shelter.doctors.include?(current_doctor)
+                    binding.pry
                     selected_shelter.doctors << current_doctor
                 end          
-                if params["breed_name"] != ''
+                if params["breed_name"] == ''
+                    binding.pry
+                    new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: params["breed_id"], shelter_id: params["shelter_id"])
+                    new_dog.save
+                else
+                    binding.pry
                     new_breed = Breed.create(name: params["breed_name"])
                     new_breed.save
                     new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: new_breed.id, shelter_id: params["shelter_id"])
                     new_dog.save
+                end
+            else
+                binding.pry
+                new_shelter = Shelter.create(params["shelter"])
+                new_shelter.save
+                new_shelter.doctors << current_doctor
+                if params["breed_name"] == ''
+                    #need to check to see if breed is already in database before creating a new one
+                   new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: params["breed_id"], shelter_id: new_shelter.id)
+                    new_dog.save  
                 else
-                    new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: params["breed_id"], shelter_id: params["shelter_id"])
+                    new_breed = Breed.create(name: params["breed_name"])
+                    new_breed.save
+                    new_dog = Dog.create(name: params["dog_name"], age: params["dog_age"], breed_id: new_breed.id, shelter_id: new_shelter.id)
                     new_dog.save
                 end 
             end
